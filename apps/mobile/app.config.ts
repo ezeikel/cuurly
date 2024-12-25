7a878d2ffcfb999d585bb27c79bdbe794ed38c4b
Ezeikel Pemberton <hi@ezeikel.dev>
2024-12-25 15:50:20 +0000

    chore: added release please.

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,48 @@
+export default {
+  expo: {
+    name: "Cuurly",
+    slug: "cuurly",
+    owner: "chewybytes",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.chewybytes.cuurly",
+    },
+    android: {
+      package: "com.chewybytes.cuurly",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      eas: {
+        projectId: "682b1d91-8787-4e46-a507-1f543707fc9d",
+      },
+    },
+  },
+};

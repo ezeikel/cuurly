fa77565d04c8e3b09d40cb04bc57808ad9147ca7
Ezeikel Pemberton <hi@ezeikel.dev>
2025-01-02 12:15:52 +0000

    feat: schema update for storing user device tokens.

--- packages/db/prisma/migrations/20250102121214_init/migration.sql
+++ packages/db/prisma/migrations/20250102121214_init/migration.sql
@@ -14,18 +14,18 @@ CREATE TYPE "HairType" AS ENUM ('STRAIGHT', 'WAVY', 'CURLY', 'COILY', 'OTHER');
 CREATE TYPE "HairLength" AS ENUM ('SHORT', 'MEDIUM', 'LONG', 'EXTRALONG');
 
 -- CreateTable
-CREATE TABLE "Media" (
+CREATE TABLE "media" (
     "id" TEXT NOT NULL,
     "type" "MediaType" NOT NULL DEFAULT 'IMAGE',
     "url" TEXT NOT NULL,
     "publicId" TEXT NOT NULL,
     "postId" TEXT,
 
-    CONSTRAINT "Media_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "media_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Profile" (
+CREATE TABLE "profiles" (
     "id" TEXT NOT NULL,
     "bio" TEXT NOT NULL DEFAULT '',
     "website" TEXT,
@@ -33,7 +33,7 @@ CREATE TABLE "Profile" (
     "hairType" "HairType",
     "hairLength" "HairLength",
 
-    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -107,19 +107,34 @@ CREATE TABLE "users" (
 );
 
 -- CreateTable
+CREATE TABLE "devices" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "devices_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "_PostProducts" (
     "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_PostProducts_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateTable
 CREATE TABLE "_UserFollows" (
     "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_UserFollows_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Profile_pictureId_key" ON "Profile"("pictureId");
+CREATE UNIQUE INDEX "profiles_pictureId_key" ON "profiles"("pictureId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
@@ -131,22 +146,16 @@ CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
 CREATE UNIQUE INDEX "users_profileId_key" ON "users"("profileId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_PostProducts_AB_unique" ON "_PostProducts"("A", "B");
-
--- CreateIndex
 CREATE INDEX "_PostProducts_B_index" ON "_PostProducts"("B");
-
--- CreateIndex
-CREATE UNIQUE INDEX "_UserFollows_AB_unique" ON "_UserFollows"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_UserFollows_B_index" ON "_UserFollows"("B");
 
 -- AddForeignKey
-ALTER TABLE "Media" ADD CONSTRAINT "Media_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "media" ADD CONSTRAINT "media_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Profile" ADD CONSTRAINT "Profile_pictureId_fkey" FOREIGN KEY ("pictureId") REFERENCES "Media"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_pictureId_fkey" FOREIGN KEY ("pictureId") REFERENCES "media"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "comments" ADD CONSTRAINT "comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -164,7 +173,10 @@ ALTER TABLE "likes" ADD CONSTRAINT "likes_postId_fkey" FOREIGN KEY ("postId") RE
 ALTER TABLE "posts" ADD CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "users" ADD CONSTRAINT "users_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "users" ADD CONSTRAINT "users_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "profiles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "devices" ADD CONSTRAINT "devices_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_PostProducts" ADD CONSTRAINT "_PostProducts_A_fkey" FOREIGN KEY ("A") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
